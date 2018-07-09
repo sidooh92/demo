07e31121dd3afc82f5a3da09fb4811f8000f90f9
robert <robert.piotrowski92@gmail.com>
2018-07-09 17:59:04 +0200

    init

---
+++ src/main/resources/db/create-table-oauth.sql
@@ -0,0 +1,15 @@
+CREATE TABLE oauth_access_token (
+  token_id VARCHAR(255),
+  token LONGVARBINARY,
+  authentication_id VARCHAR(255) PRIMARY KEY,
+  user_name VARCHAR(255),
+  client_id VARCHAR(255),
+  authentication LONGVARBINARY,
+  refresh_token VARCHAR(255)
+);
+
+CREATE TABLE oauth_refresh_token (
+  token_id VARCHAR(255),
+  token LONGVARBINARY,
+  authentication LONGVARBINARY
+);
---
+++ src/main/resources/db/insert-users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users  VALUES
+	('admin', '$2a$10$d8gv6jyA/He8va6QMSEEmuwpQM1XFKgEgj8TTGxJ13lHBQ6ftT9Bm', 1,1,0);
+
+	 INSERT INTO users VALUES
+	('test', '$2a$10$u3AJC2e8fQ7bapCZh6I6Re4siOLimyBkPp.E//Ae07CSdW1SrRrFu', 1,1,0);
